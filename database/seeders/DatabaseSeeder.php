c44acd5fbce0e041e473347168df8223875214fb
alifrachmat2002 <aaliplagi@gmail.com>
2024-04-22 21:38:03 +0700

    Create and setup Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,13 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        User::factory(10)->create();
+
+        User::create([
+            'name' => 'Alif Rachmat',
+            'email' => 'alip@gmail.com',
+            'password' => bcrypt('password')
+        ]);
     }
 }
