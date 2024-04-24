20d67d3248b65d4e83c059bff4a61d741194638a
alifrachmat2002 <aaliplagi@gmail.com>
2024-04-24 14:58:15 +0700

    Make Jabatan Controller

---
+++ app/Http/Controllers/JabatanController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jabatan;
+use Illuminate\Http\Request;
+
+class JabatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // ddd($request);
+        $validatedData = request()->validate([
+            'nama_jabatan' => 'required',
+            'unit_kerja' => 'required'
+        ]);
+
+        Jabatan::create($validatedData);
+
+        return back()->with('success', 'Data Jabatan berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Jabatan $jabatan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jabatan $jabatan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Jabatan $jabatan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jabatan $jabatan)
+    {
+        //
+    }
+}
