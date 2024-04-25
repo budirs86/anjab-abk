8688db6876818792ef4dad7ac1b61cb74f2d2824
alifrachmat2002 <aaliplagi@gmail.com>
2024-04-25 14:20:53 +0700

    Fix Navbar link misplacement

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -73,8 +73,8 @@
                     </p>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item me-3 my-1" href="/anjab/jabatan">Entry Data Jabatan</a></li>
-                        <li><a class="dropdown-item me-3 my-1" href="/anjab/analisis-jabatan">Entry Biodata Jabatan</a></li>
-                        <li><a class="dropdown-item me-3 my-1" href="#">Entry Analisis Jabatan</a></li>
+                        <li><a class="dropdown-item me-3 my-1" href="">Entry Biodata Jabatan</a></li>
+                        <li><a class="dropdown-item me-3 my-1" href="/anjab/analisis-jabatan">Entry Analisis Jabatan</a></li>
                         
                     </ul>
                     </li>
