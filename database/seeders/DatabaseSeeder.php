0604917a6681aa95e0a9688f8f9e1039681c9a87
alifrachmat2002 <aaliplagi@gmail.com>
2024-04-29 14:51:15 +0700

    Add code to seed Golongan, Eselon, JenisJabatan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Eselon;
+use App\Models\Golongan;
+use App\Models\JenisJabatan;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -27,5 +30,119 @@ class DatabaseSeeder extends Seeder
             'email' => 'alip@gmail.com',
             'password' => bcrypt('password')
         ]);
+
+        $jenis_jabatan = [
+            "Jabatan Pimpinan Tinggi Utama",
+            "Jabatan Pimpinan Tinggi Madya",
+            "Jabatan Pimpinan Tinggi Pratama",
+            "Jabatan Administrasi Administrator",
+            "Jabatan Administrasi Pengawas",
+            "Jabatan Administrasi Pelaksana",
+            "Jabatan Fungsional",
+        ];
+
+        $eselon = [
+            "Eselon I",
+            "Eselon IA",
+            "Eselon IB",
+            "Eselon II",
+            "Eselon IIA",
+            "Eselon IIB",
+            "Eselon III",
+            "Eselon IIIA",
+            "Eselon IIIB",
+        ];
+
+        $golongan_data = array(
+        // Golongan I (Juru)
+        array(
+            "golongan" => "IA",
+            "nama" => "Juru Muda",
+        ),
+        array(
+            "golongan" => "IB",
+            "nama" => "Juru Muda Tingkat I",
+        ),
+        array(
+            "golongan" => "IC",
+            "nama" => "Juru",
+        ),
+        array(
+            "golongan" => "ID",
+            "nama" => "Juru Tingkat I",
+        ),
+        
+        // Golongan II (Pengatur)
+        array(
+            "golongan" => "IIA",
+            "nama" => "Pengatur Muda",
+        ),
+        array(
+            "golongan" => "IIB",
+            "nama" => "Pengatur Muda Tingkat I",
+        ),
+        array(
+            "golongan" => "IIC",
+            "nama" => "Pengatur",
+        ),
+        array(
+            "golongan" => "IID",
+            "nama" => "Pengatur Tingkat I",
+        ),
+        
+        // Golongan III (Penata)
+        array(
+            "golongan" => "IIIA",
+            "nama" => "Penata Muda",
+        ),
+        array(
+            "golongan" => "IIIB",
+            "nama" => "Penata Muda Tingkat I",
+        ),
+        array(
+            "golongan" => "IIIC",
+            "nama" => "Penata",
+        ),
+        array(
+            "golongan" => "IIID",
+            "nama" => "Penata Tingkat I",
+        ),
+        
+        // Golongan IV (Pembina)
+        array(
+            "golongan" => "IVA",
+            "nama" => "Pembina",
+        ),
+        array(
+            "golongan" => "IVB",
+            "nama" => "Pembina Tingkat I",
+        ),
+        array(
+            "golongan" => "IVC",
+            "nama" => "Pembina Muda",
+        ),
+        array(
+            "golongan" => "IVD",
+            "nama" => "Pembina Muda Tingkat I",
+        ),
+        array(
+            "golongan" => "IVE",
+            "nama" => "Pembina Utama",
+        ),
+        );
+
+        foreach ($jenis_jabatan as $jenis) {
+            JenisJabatan::create([
+                'jenis_jabatan' => $jenis
+            ]);
+        }
+        foreach ($eselon as $es) {
+            Eselon::create([
+                'eselon' => $es
+            ]);
+        }
+        foreach ($golongan_data as $golongan) {
+            Golongan::create($golongan);
+        }
     }
 }
