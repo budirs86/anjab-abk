00c92a29095f5599a52270e182fb7b7c937c4c45
alifrachmat2002 <aaliplagi@gmail.com>
2024-06-14 14:22:50 +0700

    feat : add routes for "Buat Ajuan Baru", "Peta Jabatan", "Ajuans", "Ajuan" pages

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\JabatanController;
 use App\Http\Controllers\LoginController;
 use App\Models\Jabatan;
 use App\Models\JenisJabatan;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,3 +20,43 @@ Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 
 Route::resource('/anjab/data-jabatan/', JabatanController::class)->middleware('auth');
 Route::resource('/anjab/analisis-jabatan/', AnalisisJabatanController::class)->middleware('auth');
+
+Route::get('/anjab/buat-ajuan',function() {
+    return view('anjab.buat-ajuan',[
+        'title' => ''
+    ]);
+})->name('anjab.buat-ajuan')->middleware('auth');
+
+Route::get('/petajabatan', function() {
+
+    $jabatans = Jabatan::tree()->get()->toTree();
+    // dd($jabatans);
+    return view("anjab.petajabatan.petajabatan",[
+        'title' => "Peta Jabatan",
+        'jabatans' => $jabatans
+    ]);
+})->middleware('auth');
+
+Route::view('anjab/ajuans','anjab.ajuans',[
+    'title' => 'Ajuan Jabatan'
+])->name('anjab.ajuans');
+
+Route::get('/anjab/ajuan/{id}',function($id) {
+    $jabatans = Jabatan::tree()->get()->toTree();
+
+    return view('anjab.ajuan',[
+        'title' => 'Ajuan Jabatan',
+        'jabatans' => $jabatans,
+        'editable' => false
+    ]);
+});
+Route::get('/anjab/ajuan/{id}/edit',function($id) {
+    // $jabatans = Jabatan::tree()->get()->toTree();
+    $jabatans = Jabatan::all();
+
+    return view('anjab.ajuan',[
+        'title' => 'Ajuan Jabatan',
+        'jabatans' => $jabatans,
+        'editable' => true
+    ]);
+});
