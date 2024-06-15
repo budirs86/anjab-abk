cceaa84f464f52a86ca2ffcf2f54e3b5bbe4539e
alifrachmat2002 <aaliplagi@gmail.com>
2024-06-15 12:42:05 +0700

    feat: Add breadcrumbs to Ajuans view

--- resources/views/anjab/ajuans.blade.php
+++ resources/views/anjab/ajuans.blade.php
@@ -1,6 +1,9 @@
 @extends('anjab.layouts.main')
 
 @section('content')
+<div class="">
+    {{ Breadcrumbs::render('ajuan-analisis-jabatan') }}
+</div>
         <div class="card-head mb-3">
                 <h1 class="fw-light fs-4 d-inline nav-item">Daftar Ajuan Analisis Jabatan</h1>                
         </div>
@@ -24,11 +27,17 @@
                 <tr>
                     <td>{{ $i }}</td>
                     <td>{{ $i + 2020}}</td>
-                    <td>{{ ($i % 2 == 0) ? "Disetujui": "Revisi" }}</td>
+                    @if ($i % 2 == 0)
+                        <td><h5><span class="badge rounded-pill text-bg-success">Disetujui</span></h5></td=>
+                    @else
+                        <td><h5><span class="badge rounded-pill text-bg-danger">Revisi</span></h5></td>
+                        
+                    @endif
+                    {{-- <td>{{  ? <p class="bad"></p> : "Revisi" }}</td> --}}
                     <td>
-                        <a href="/anjab/ajuan/{{ $i }}?periode={{ $i+2020 }}" class="btn btn-primary">Lihat Ajuan</a>
+                        <a href="/anjab/ajuan/{{ $i }}?periode={{ $i+2020 }}" class="btn btn-primary m-0 pt-1" ><i data-feather="eye" class="m-0 p-0"></i></a>
                         @if ($i % 2 != 0)
-                            <a href="/anjab/ajuan/1/edit" class="btn btn-warning">Edit</a>
+                            <a href="/anjab/ajuan/1/edit" class="btn btn-warning"><i data-feather="edit"></i></a>
                         @else
                             <a href="/abk/ajuan" class="btn btn-success" aria-disabled="true">Buat Ajuan ABK</a>
                         @endif
