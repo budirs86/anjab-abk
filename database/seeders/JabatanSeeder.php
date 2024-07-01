8e3f8d0c0746976de6663bdcf9884b7583b4acf0
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-07-01 13:33:41 +0700

    DB: Fakultas Teknik
    - create fakultas_teknik.json
    - update JabatanSeeder to handle data from multiple sources

--- database/seeders/JabatanSeeder.php
+++ database/seeders/JabatanSeeder.php
@@ -13,7 +13,9 @@ class JabatanSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = json_decode(file_get_contents(database_path('seeders/data/jabatan.json')), true);
+        $data_fakultas_teknik = json_decode(file_get_contents(database_path('seeders/data/jabatan/fakultas_teknik.json')), true);
+
+        $data = array_merge($data_fakultas_teknik);
 
         foreach ($data as $jabatan) {
             Jabatan::create($jabatan);
