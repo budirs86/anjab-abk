9345a6c41a1c1a1ea0f0a989bac78463aecb2b6d
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-07-01 10:53:58 +0700

    Chore: Update attribute name in table-row.blade from $jabatan->nama_jabatan to $jabatan->nama

--- resources/views/components/table-row.blade.php
+++ resources/views/components/table-row.blade.php
@@ -14,20 +14,20 @@
     <td>// K-123 //</td>
     <td class="d-flex justify-content-between">
         @if ($editable && !$abk)
-            <p class="" href="/anjab/analisis-jabatan/create" style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama_jabatan }}</p>
+            <p class="" href="/anjab/analisis-jabatan/create" style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama }}</p>
             <div class="">
-                <a href="{{ route('create',['nama_jabatan' => $jabatan->nama_jabatan]) }}" class="btn btn-sm btn-primary ms-auto add-button"><img width="20px" data-feather="edit-3"></img> Ubah Informasi Jabatan</a>
+                <a href="{{ route('create',['nama_jabatan' => $jabatan->nama]) }}" class="btn btn-sm btn-primary ms-auto add-button"><img width="20px" data-feather="edit-3"></img> Ubah Informasi Jabatan</a>
                 <button class="btn btn-sm btn-success ms-auto add-button" data-bs-toggle="modal" data-bs-target="#modalJabatan" id="addButton" data-bs-atasan="{{ $jabatan->id }}"><img width="20px" data-feather="plus"></img> Tambah Jabatan Bawahan</button>
             </div>
         @elseif(!$editable && !$abk)
-            <p style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama_jabatan }}</p>
+            <p style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama }}</p>
             <a href="" class="btn btn-sm btn-primary">Lihat Informasi Jabatan</a>
             
         @elseif($editable && $abk)
-            <p style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama_jabatan }}</p>
+            <p style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama }}</p>
             <a href="{{ route('abk.data-abk') }}" class="btn btn-sm btn-primary add-button"><img width="20px" data-feather="edit-3"></img> Isi Informasi ABK</a>
         @else
-            <p style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama_jabatan }}</p>
+            <p style="margin-left: {{ $jabatan->depth * 25 }}px;"><img width="20px" data-feather="corner-down-right"></img> {{ $jabatan->nama }}</p>
             <a href="{{ route('abk.jabatan.show',['id' => request()->periode, 'jabatan' => $jabatan->id ]) }}" class="btn btn-sm btn-primary">Lihat Informasi ABK</a>
         @endif
         {{-- <button class="btn btn-success ms-2"> Tambah Jabatan</button> --}}
