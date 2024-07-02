f8dc1efee9e4f35676348d5bbc91eeb999a602e1
alifrachmat2002 <aaliplagi@gmail.com>
2024-07-02 15:42:57 +0700

    chore: Add Font Awesome CDN for icons

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -46,6 +46,8 @@
     <script src="https://cdn.jsdelivr.net/gh/xcash/bootstrap-autocomplete@v2.3.7/dist/latest/bootstrap-autocomplete.min.js"></script>
     <script src="bootstrap-autocomplete.min.js"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
     {{-- feathericons js --}}
     <script>feather.replace();</script>
 </body>
