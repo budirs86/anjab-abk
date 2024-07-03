9dd16c29513d7365b51f8b42c992027f2b2ccc2f
alifrachmat2002 <aaliplagi@gmail.com>
2024-07-03 20:46:44 +0700

    DB : UpayaFisik and cache table migration
    - create UpayaFisik Model, migration, and seeder
    - create upaya_fisik.json to store upaya fisik seed data
    - call UpayaFisikSeeder in DatabaseSeeder
    - reinclude previously deleted create_cache_table migration due to spatie/laravel_permission package requirements

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             GolonganSeeder::class,
             UnitKerjaSeeder::class,
             JabatanSeeder::class,
+            UpayaFisikSeeder::class,
         ]);
     }
 }
