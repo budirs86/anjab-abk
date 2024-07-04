77a7fda3575f63c25e8bd5d1a53efd0328859de4
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-07-04 11:20:54 +0700

    Feat: Display Data in Ajuan Anjab Index
    - Update ajuan blade
    - Update anjabIndex() in AjuanController
    - Create anjabStore() in AjuanController

--- app/Http/Controllers/AjuanController.php
+++ app/Http/Controllers/AjuanController.php
@@ -9,6 +9,7 @@ use App\Models\Golongan;
 use App\Models\Jabatan;
 use App\Models\JenisJabatan;
 use App\Models\UnitKerja;
+use App\Models\Verifikasi;
 use Illuminate\Http\Request;
 
 class AjuanController extends Controller
@@ -16,8 +17,9 @@ class AjuanController extends Controller
     public function anjabIndex()
     {
         $title = 'Ajuan Jabatan';
+        $ajuans = Ajuan::where('jenis', 'anjab')->get();
 
-        return view('anjab/ajuans', compact('title'));
+        return view('anjab.ajuans', compact('title', 'ajuans'));
     }
 
     public function anjabCreate()
@@ -32,7 +34,7 @@ class AjuanController extends Controller
         return view('anjab.buat-ajuan', compact('title', 'jabatans', 'jenisJabatan', 'eselon', 'golongan', 'unitKerjas', 'buttons'));
     }
 
-    public function anjabStore($jabatans)
+    public function anjabStore(Request $request)
     {
         Ajuan::create([
             'tahun' => now()->year,
@@ -40,11 +42,14 @@ class AjuanController extends Controller
             'jenis' => 'anjab'
         ]);
 
-        foreach ($jabatans as $jabatan) {
+        $jabatans = $request->input('jabatans');
+        foreach ($jabatans as $jabatan_id) {
             AjuanJabatan::create([
                 'ajuan_id' => Ajuan::latest()->first()->id,
-                'jabatan_id' => $jabatan->id
+                'jabatan_id' => $jabatan_id
             ]);
         }
+
+        return redirect()->route('anjab.ajuan.index')->with('success', 'Ajuan Jabatan berhasil diajukan');
     }
 }
--- resources/views/anjab/ajuans.blade.php
+++ resources/views/anjab/ajuans.blade.php
@@ -1,45 +1,47 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="">
-    {{ Breadcrumbs::render('ajuan-analisis-jabatan') }} 
-</div>
-        <div class="card-head mb-3">
-                <h1 class="fw-light fs-4 d-inline nav-item">Daftar Ajuan Analisis Jabatan</h1>                
-        </div>
-        <div class="alert alert-dismissible alert-success fade show">
-            <p class="m-0">Ajuan berhasil disimpan!</p>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        <div class="card dropdown-divider mb-3"></div>
-        <a type="button" class="btn-primary btn mb-3" href="{{ route('anjab.ajuan.create') }}"><i data-feather="plus"></i> Buat Ajuan Baru</a>
-        <table class="table table-striped table-bordered table-responsive">
-            <thead>
+    <div class="">
+        {{ Breadcrumbs::render('ajuan-analisis-jabatan') }}
+    </div>
+    <div class="card-head mb-3">
+        <h1 class="fw-light fs-4 d-inline nav-item">Daftar Ajuan Analisis Jabatan</h1>
+    </div>
+    <div class="alert alert-dismissible alert-success fade show">
+        <p class="m-0">Ajuan berhasil disimpan!</p>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div class="card dropdown-divider mb-3"></div>
+    <a type="button" class="btn-primary btn mb-3" href="{{ route('anjab.ajuan.create') }}"><i data-feather="plus"></i> Buat
+        Ajuan Baru</a>
+    <table class="table table-striped table-bordered table-responsive">
+        <thead>
             <tr>
                 <th style="width: 10%">No</th>
                 <th>Periode</th>
                 <th>Status</th>
                 <th>Catatan</th>
             </tr>
-            </thead>
-            <tbody>
+        </thead>
+        <tbody>
             {{-- Loop through the ajuan data and display each row --}}
-            
+
             {{-- @foreach ($ajuanData as $ajuan)
             @endforeach --}}
-            
-            @for($i = 1; $i <= 2; $i++)
+
+            @for ($i = 1; $i <= 2; $i++)
                 <tr>
                     <td>{{ $i }}</td>
                     <td class="w-25">
                         <div class="d-flex justify-content-between">
-                            <p>{{ $i + 2020}} </p>
+                            <p>{{ $i + 2020 }} </p>
                             <div class="btn-group" role="group" aria-label="Basic example">
-                                <a href="/anjab/ajuan/{{ $i }}?periode={{ $i+2020 }}" class="btn btn-outline-primary">Lihat</a>
-                                @if ($i % 2 != 0)                                  
+                                <a href="/anjab/ajuan/{{ $i }}?periode={{ $i + 2020 }}"
+                                    class="btn btn-outline-primary">Lihat</a>
+                                @if ($i % 2 != 0)
                                 @else
-                                    <a href="/abk/ajuan/create" class="btn btn-outline-success" aria-disabled="true">Buat Ajuan ABK</a>
-                                    
+                                    <a href="/abk/ajuan/create" class="btn btn-outline-success" aria-disabled="true">Buat
+                                        Ajuan ABK</a>
                                 @endif
                             </div>
                         </div>
@@ -51,7 +53,7 @@
                                     <img width="20px" data-feather="check-circle" class="m-0 p-0 me-2"></img>
                                     <p class="m-0 p-0">Disetujui</p>
                                 </div>
-                                <hr>  
+                                <hr>
                                 <p class="m-0 p-0">Manajer Tata Usaha/Kepegawaian</p>
                             </div>
                             <div class="alert alert-info w-100">
@@ -59,20 +61,20 @@
                                     <img width="20px" data-feather="clock" class="m-0 p-0 me-2"></img>
                                     <p class="m-0 p-0">Menunggu Diperiksa</p>
                                 </div>
-                                <hr>  
+                                <hr>
                                 <p class="m-0 p-0">Kepala Biro, Wakil Dekan 2, Sekretaris Lembaga</p>
                             </div>
                         </td>
                         <td>Tidak ada catatan.</td>
-                        @else
+                    @else
                         <td class="">
                             <div class="alert alert-warning w-100">
-                                    <div class="alert-heading d-flex">
-                                        <img width="20px" data-feather="alert-triangle" class="m-0 p-0 me-2"></img>
-                                        <p class="m-0 p-0">Perlu Perbaikan</p>
-                                    </div>
-                                    <hr>  
-                                    <p class="m-0 p-0">Kepala Biro, Wakil Dekan 2, Sekretaris Lembaga</p>
+                                <div class="alert-heading d-flex">
+                                    <img width="20px" data-feather="alert-triangle" class="m-0 p-0 me-2"></img>
+                                    <p class="m-0 p-0">Perlu Perbaikan</p>
+                                </div>
+                                <hr>
+                                <p class="m-0 p-0">Kepala Biro, Wakil Dekan 2, Sekretaris Lembaga</p>
                             </div>
 
                         </td>
@@ -83,14 +85,49 @@
                                 <li>Lorem ipsum dolor doloran</li>
                             </ul>
                         </td>
-                        
                     @endif
                     {{-- <td>{{  ? <p class="bad"></p> : "Revisi" }}</td> --}}
                 </tr>
             @endfor
+            @foreach ($ajuans as $ajuan)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>
+                        <div class="d-flex justify-content-between">
+                            <p>{{ $ajuan->tahun }}</p>
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <a href="/anjab/ajuan/{{ $ajuan->id }}" class="btn btn-outline-primary">Lihat</a>
+                                <a href="/abk/ajuan/create" class="btn btn-outline-success" aria-disabled="true">Buat Ajuan
+                                    ABK</a>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="alert alert-success w-100">
+                            <div class="alert-heading d-flex">
+                                <img width="20px" data-feather="check-circle" class="m-0 p-0 me-2"></img>
+                                <p class="m-0 p-0">Disetujui</p>
+                            </div>
+                            <hr>
+                            <p class="m-0 p-0">Manajer Tata Usaha/Kepegawaian</p>
+                        </div>
+                        <div class="alert alert-info w-100">
+                            <div class="alert-heading d-flex">
+                                <img width="20px" data-feather="clock" class="m-0 p-0 me-2"></img>
+                                <p class="m-0 p-0">Menunggu Diperiksa</p>
+                            </div>
+                            <hr>
+                            <p class="m-0 p-0">Kepala Biro, Wakil Dekan 2, Sekretaris Lembaga</p>
+                        </div>
+                    </td>
+                    <td>
+                        {{ $ajuan->verifikasis->latest()->first()->catatan ? $ajuan->verifikasis->latest()->first()->catatan : 'Tidak ada catatan.' }}
+                    </td>
+                </tr>
+            @endforeach
             {{-- please  --}}
-            </tbody>
-        </table>
-        {{-- make a kembali button --}}
-        <a href="{{ route('home') }}" class="btn btn-secondary"><i data-feather="chevron-left"></i> Kembali</a>
-@endsection 
+        </tbody>
+    </table>
+    {{-- make a kembali button --}}
+    <a href="{{ route('home') }}" class="btn btn-secondary"><i data-feather="chevron-left"></i> Kembali</a>
+@endsection
