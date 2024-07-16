36a1746a08ca13636c46febc0937bfd4651fae99
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-07-16 14:15:15 +0700

    DB: Create model and migration for JabatanDiajukan

---
+++ app/Models/JabatanDiajukan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JabatanDiajukan extends Model
+{
+    use HasFactory;
+
+    // table name is 'jabatan_diajukan'
+    protected $table = 'jabatan_diajukan';
+}
---
+++ database/migrations/2024_07_16_071023_create_jabatan_diajukan_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('jabatan_diajukan', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('parent_id')->nullable();
+      $table->foreignId('ajuan_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('jenis_jabatan_id')->constrained()->nullable();
+      $table->foreignId('unit_kerja_id')->constrained()->nullable();
+      $table->string('nama');
+      $table->string('kode');
+      $table->integer('kelas_jabatan')->nullable();
+      $table->text('ikhtisar')->nullable();
+      $table->text('prestasi')->nullable();
+      $table->text('tanggung_jawab')->nullable();
+      $table->timestamps();
+
+      $table->foreign('parent_id')->references('id')->on('jabatan_diajukan')->onDelete('cascade');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('jabatan_diajukans');
+  }
+};
