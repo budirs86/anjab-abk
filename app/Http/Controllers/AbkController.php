af48a6818972b237d6c5aafcb85b9db40647bec8
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-07-25 16:22:03 +0700

    Feat: Create ABK Ajuan(s) After Clicking Button on Anjab Ajuan
    - Setup web.php
    - Create AbkController@storeAjuan
    - Create model and migration for AjuanUnitKerja
    - Define relationship between models: UnitKerja, JabatanDiajukan
    - Update anjab ajuans blade

--- app/Http/Controllers/AbkController.php
+++ app/Http/Controllers/AbkController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ajuan;
+use App\Models\AjuanUnitKerja;
 use App\Models\Jabatan;
+use App\Models\JabatanDiajukan;
+use App\Models\UnitKerja;
 use Illuminate\Http\Request;
 
 class AbkController extends Controller
@@ -24,4 +27,30 @@ class AbkController extends Controller
             'jabatans' => Jabatan::all()
         ]);
     }
+
+  public function storeAjuan(Ajuan $ajuan)
+  {
+    // get all jabatan from the anjab
+    $jabatans = JabatanDiajukan::where('ajuan_id', $ajuan->id)->get();
+    // get all unique unit kerja from the jabatan
+    $unitKerjas = $jabatans->map(function ($jabatan) {
+      return $jabatan->unitKerja;
+    })->unique();
+    // for each unit kerja on the anjab, 
+    // create ajuan with current year as 'tahun' and abk as 'jenis'
+    foreach ($unitKerjas as $unitKerja) {
+      $ajuan = Ajuan::create([
+        'tahun' => now()->year,
+        'jenis' => 'abk'
+      ]);
+
+      // also create ajuan unit kerja with the ajuan id and unit kerja id
+      AjuanUnitKerja::create([
+        'ajuan_id' => $ajuan->id,
+        'unit_kerja_id' => $unitKerja->id
+      ]);
+    }
+
+    return redirect()->route('abk.ajuans');
+  }
 }
