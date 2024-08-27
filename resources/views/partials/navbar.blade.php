a14e5e53d5a7d8cea499ae2dd710683093a81c0e
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-08-27 15:48:55 +0700

    feat: create different navbar display for superadamin
    - create new conditional
    - delete old conditional

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -31,14 +31,6 @@
                         <a class="nav-link text-white d-inline" href="/login" role="button" aria-expanded="false">
                             Login
                         </a>
-                        {{-- <a
-                            class="nav-link text-white d-inline"
-                            href="#"
-                            role="button"
-                            aria-expanded="false"
-                        >
-                            Register
-                        </a> --}}
                     </li>
                 @endauth
             </ul>
@@ -48,50 +40,48 @@
         <div class="container-fluid mx-3">
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <ul class="navbar-nav gap-3">
-                    <li><a class="nav-link" href="/"><img data-feather="home" width="20px"> Dashboard</a></li>
-                    <li class="nav-item dropdown">
-                        <p class="nav-link " href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <img src="" width=20px data-feather="edit" alt="">Anjab
-                        </p>
-                        <ul class="dropdown-menu">
-                            @can('make anjab')
-                                <li><a class="dropdown-item me-3 my-1" href="{{ route('anjab.ajuan.create') }}">Buat Ajuan
-                                        Informasi Jabatan Baru</a></li>
-                            @endcan
-                            <li><a class="dropdown-item me-3 my-1" href="{{ route('anjab.ajuan.index') }}">Lihat
-                                    Ajuan</a></li>
-                            {{-- <li><a class="dropdown-item me-3 my-1" href="">Entry Biodata Jabatan</a></li>
-                        <li><a class="dropdown-item me-3 my-1" href="/anjab/analisis-jabatan">Entry Analisis Jabatan</a></li> --}}
-
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown"><img
-                                data-feather="edit" width="20px"></img> ABK</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item me-3 my-1"
-                                    href="{{ route('anjab.ajuan.index', ['abk' => true]) }}">Buat Ajuan Baru</a></li>
-                            <li><a class="dropdown-item me-3 my-1" href="{{ route('abk.ajuans') }}">Lihat Ajuan</a></li>
-                        </ul>
-                    </li>
-
-                    <li><a class="nav-link" href="{{ route('laporan.index') }}"><img data-feather="file"
-                                width="20px"></img> Laporan</a>
-                    </li>
                     @if (auth()->user()->hasRole('superadmin'))
+                        <li><a class="nav-link" href="/"><img data-feather="home" width="20px"> Dashboard</a>
+                        <li><a class="nav-link" href="{{ route('admin.users.index') }}"><img data-feather="user"
+                                    width="20px"></img>User</a>
+                        </li>
+                        <li><a class="nav-link" href="/"><img data-feather="edit" width="20px">Tugas
+                                Tambahan</a></li>
+                        <li><a class="nav-link" href="{{ route('admin.jabatans.index') }}"><img data-feather="file"
+                                    width="20px">Jabatan</a></li>
+                        <li><a class="nav-link" href="/"><img data-feather="home" width="20px">Unsur & Unit Kerja</a>
+                        </li>
+                    @else
+                        <li><a class="nav-link" href="/"><img data-feather="home" width="20px"> Dashboard</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <p class="nav-link " href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <img src="" width=20px data-feather="edit" alt="">Anjab
+                            </p>
+                            <ul class="dropdown-menu">
+                                @can('make anjab')
+                                    <li><a class="dropdown-item me-3 my-1" href="{{ route('anjab.ajuan.create') }}">Buat
+                                            Ajuan
+                                            Informasi Jabatan Baru</a></li>
+                                @endcan
+                                <li><a class="dropdown-item me-3 my-1" href="{{ route('anjab.ajuan.index') }}">Lihat
+                                        Ajuan</a></li>
+                            </ul>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown"><img
-                                    data-feather="user" width="20px"></img> Admin</a>
+                                    data-feather="edit" width="20px"></img> ABK</a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item me-3 my-1"
-                                        href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                                <li><a class="dropdown-item me-3 my-1" href="{{ route('admin.users.index') }}">Manage
-                                        Users</a></li>
-                                <li><a class="dropdown-item me-3 my-1" href="{{ route('admin.jabatans.index') }}">Manage
-                                        Jabatans</a></li>
-
+                                        href="{{ route('anjab.ajuan.index', ['abk' => true]) }}">Buat Ajuan Baru</a>
+                                </li>
+                                <li><a class="dropdown-item me-3 my-1" href="{{ route('abk.ajuans') }}">Lihat Ajuan</a>
+                                </li>
                             </ul>
+                        </li>
+                        <li><a class="nav-link" href="{{ route('laporan.index') }}"><img data-feather="file"
+                                    width="20px"></img> Laporan</a>
                         </li>
                     @endif
                 </ul>
